9cc9ef045c3de980dbbf7343b62740b1ec387f16
Fagner Joaquim <fagneralves.xone@gmail.com>
2022-04-16 20:35:14 -0300

    Aula-03

--- estrutura-dados/js/array.js
+++ estrutura-dados/js/array.js
@@ -6,4 +6,15 @@ avgTempo[2] = 42;
 avgTempo[3] = 38;
 avgTempo[4] = 25.5;
 
-console.log(avgTempo[4])
+console.log(avgTempo[4]);
+
+var diasSemana = new Array('Domingo', 'Segunda-Feira');
+
+diasSemana [2] = 'Terca-Feira';
+diasSemana [3] = 'Quarta Feira';
+diasSemana [4] = 'Quinta-Feira';
+diasSemana [5] = 'Sexta-Feira';
+diasSemana [6] = 'Sábado';
+
+console.log(diasSemana.length);
+console.log(diasSemana[0])
